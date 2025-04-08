8d55b1c48daab48c96bdab1f150470ee6daadb92
Jose Mario <josemariobernardes@gmail.com>
2025-04-08 13:59:08 -0300

    Adiciona envio de e-mail e ajustes finais para cron job

--- dashboard/management/commands/coletar_preco.py
+++ dashboard/management/commands/coletar_preco.py
@@ -3,33 +3,32 @@ from dashboard.models import PrecoBoi
 from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
 from django.core.mail import send_mail
 from django.conf import settings
-import sqlite3
 
 class Command(BaseCommand):
     help = 'Executa o scraper de preço do boi SP e envia um e-mail de confirmação'
 
     def handle(self, *args, **kwargs):
         preco = None
-        options = Options()
-        options.add_argument("--headless=new")
+
+        options = FirefoxOptions()
+        options.add_argument("--headless")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
 
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+        driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()), options=options)
         driver.get("https://portal.datagro.com/pt/livestock")
 
         try:
             WebDriverWait(driver, 20).until(
                 EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'Indicador do Boi SP')]"))
             )
-
             elemento = driver.find_element(By.XPATH, "//*[contains(text(), 'Indicador do Boi SP')]")
             linha = elemento.find_element(By.XPATH, "./ancestor::tr")
             colunas = linha.find_elements(By.TAG_NAME, "td")
